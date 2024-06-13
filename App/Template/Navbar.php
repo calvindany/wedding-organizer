3b405b5fa3f6dcfc2f531d30545cf09661ec3a32
calvindany <calvindany1102@gmail.com>
2024-06-13 13:37:37 +0700

    fixed: text color on login and home admin

--- App/Template/Navbar.php
+++ App/Template/Navbar.php
@@ -5,7 +5,7 @@
     
     $isLoggedIn = isset($_SESSION['username']);
 ?>
-<nav class="navbar navbar-expand-lg shadow-sm p-4" style="background-color: #091540;">
+<nav class="navbar navbar-expand-lg shadow-sm p-4" style="background-color: #162719;">
     <div class="container d-flex justify-content-between">
         <a class="navbar-brand text-white" href="<?php echo $BASE_URL ?>">Wedding Organizer</a>
         <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,7 +16,7 @@
                 echo '<div class="collapse navbar-collapse " id="navbarNav">';
                 echo '<ul class="navbar-nav ms-auto navigation">';
                 echo '<li class="nav-item">';
-                echo '<a class="nav-link active text-white" aria-current="page" href="'. $BASE_URL . '/admin">Katalog</a>';
+                echo '<a class="nav-link active" style="color: #f2E8CF" aria-current="page" href="'. $BASE_URL . '/admin">Katalog</a>';
                 echo '</li>';
                 echo '<li class="nav-item">';
                 echo '<a class="nav-link active text-white" aria-current="page" href="'. $BASE_URL . '#">Pesanan</a>';
