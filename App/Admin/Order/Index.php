0e457f6fb24d916f1ad1000f76a27042d80b5439
Calvin Danyalson <calvindany1102@gmail.com>
2024-06-14 21:42:22 +0700

    fixed: modal text wording and button

--- App/Admin/Order/Index.php
+++ App/Admin/Order/Index.php
@@ -72,8 +72,8 @@
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-primary px-4" data-bs-dismiss="modal">OK</button>
+                    <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
                 </div>
             </div>
         </div>
