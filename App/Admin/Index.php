0b824c45f3595e6de08125160ef9568d1a65b18d
Calvin Danyalson <calvindany1102@gmail.com>
2024-06-14 14:20:51 +0700

    fixed: truncate text styling on admin home page

--- App/Admin/Index.php
+++ App/Admin/Index.php
@@ -22,7 +22,7 @@
                         echo "  <div class='card-body'>";
                         echo "      <h5 class='card-title'>". $row["product_name"] . "</h5>";
                         echo "      <p class='card-text'>IDR " . $row["price"] . "</p>";
-                        echo "      <p class='card-text truncate-multiline'>" . $row["description"] . "</p>";
+                        echo "      <div class='card-text truncate-multiline'>" . $row["description"] . "</div>";
                         echo "      <a href='". $BASE_URL . "admin/catalogue/detail/" . $row["pk_tb_catalogue"] ."' class='btn px-3 btn-primary'>Detail</a>";
                         echo "  </div>";
                         echo "</div>";
