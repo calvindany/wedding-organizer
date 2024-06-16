9cfeb600edabb08ed0e323630f1fcd51658c73f8
Calvin Danyalson <calvindany1102@gmail.com>
2024-06-16 10:02:00 +0700

    fixed: color styling on navbar admin and user

--- App/Template/Navbar.php
+++ App/Template/Navbar.php
@@ -5,7 +5,7 @@
     
     $isLoggedIn = isset($_SESSION['username']);
 ?>
-<nav class="navbar navbar-expand-lg shadow-sm p-4" style="background-color: #162719;">
+<nav class="navbar navbar-expand-lg shadow-sm p-4">
     <div class="container d-flex justify-content-between">
         <a class="navbar-brand text-white" href="<?php echo $BASE_URL ?>">Wedding Organizer</a>
         <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,19 +16,19 @@
                 echo '<div class="collapse navbar-collapse " id="navbarNav">';
                 echo '<ul class="navbar-nav ms-auto navigation">';
                 echo '<li class="nav-item">';
-                echo '<a class="nav-link active" style="color: #f2E8CF" aria-current="page" href="'. $BASE_URL . 'admin">Katalog</a>';
+                echo '<a class="nav-link  ' . ($REQUEST_URI == '/admin' ? 'active' : 'text-white') . '" aria-current="page" href="'. $BASE_URL . 'admin">Katalog</a>';
                 echo '</li>';
                 echo '<li class="nav-item">';
-                echo '<a class="nav-link active text-white" aria-current="page" href="'. $BASE_URL . 'admin/pesanan">Pesanan</a>';
+                echo '<a class="nav-link  ' . ($REQUEST_URI == '/admin/pesanan' ? 'active' : 'text-white') . '" aria-current="page" href="'. $BASE_URL . 'admin/pesanan">Pesanan</a>';
                 echo '</li>';
                 echo '<li class="nav-item">';
-                echo '<a class="nav-link active text-white" aria-current="page" href="'. $BASE_URL . 'admin/laporan">Laporan</a>';
+                echo '<a class="nav-link ' . ($REQUEST_URI == '/admin/laporan' ? 'active' : 'text-white') . '" aria-current="page" href="'. $BASE_URL . 'admin/laporan">Laporan</a>';
                 echo '</li>';
                 echo '<li class="nav-item">';
-                echo '<a class="nav-link active text-white" aria-current="page" href="'. $BASE_URL . 'admin/pengaturan">Pengaturan Web</a>';
+                echo '<a class="nav-link ' . ($REQUEST_URI == '/admin/pengaturan' ? 'active' : 'text-white') . '" aria-current="page" href="'. $BASE_URL . 'admin/pengaturan">Pengaturan Web</a>';
                 echo '</li>';
                 echo '<li class="nav-item">';
-                echo '<a class="nav-link active text-white" aria-current="page" href="'. $BASE_URL . 'admin/profil">Profil</a>';
+                echo '<a class="nav-link ' . ($REQUEST_URI == '/admin/profil' ? 'active' : 'text-white') . '" aria-current="page" href="'. $BASE_URL . 'admin/profil">Profil</a>';
                 echo '</li>';
                 echo '<li class="nav-item">';
                 echo '<form method="POST" action="' . $BASE_URL . 'admin/logout' .'"><button class="btn btn-danger" type="submit">Log Out</button></form>';
